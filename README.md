f39b94c7ffe280f24d96c8bfe6d3ac504669de51
nasum <nasu@scouty.co.jp>
2018-12-20 19:09:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# web_to_desktop
+
+This command display the target Web page in a new Window.
+
+## how to install
+
+```bash
+npm install
+npm link
+web_to_desktop target_web_url
+```
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const carlo = require('carlo');
+
+let url = ""
+
+if (process.argv[2]) {
+  url = process.argv[2]
+} else {
+  url = 'index.html'
+}
+
+carlo.launch().then(async app => {
+  app.on('exit', () => process.exit());
+  app.serveFolder(__dirname);
+  await app.exposeFunction('env', _ => process.env);
+  await app.load(url);
+});
