5be2a5bad5a2b345a14f2986bf833804d01efe53
nasum <nasu@scouty.co.jp>
2018-12-28 18:16:44 +0900

    you can choose chrome version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
+const path = require('path')
 const carlo = require('carlo');
+const meow = require('meow')
 
-let url = ""
+const cli = meow(`
+  Usage
+    $ web_to_desktop <input>
 
-if (process.argv[2]) {
-  url = process.argv[2]
-} else {
-  url = 'index.html'
+  Options
+    --channel, -c Browser to be used.
+`,{
+  flags: {
+    channel: {
+      type: 'string',
+      alias: 'c',
+      default: 'stable'
+    }
+  }
+})
+
+let url = cli.input[0]
+
+if (url == null) {
+  url = path.join(__dirname, 'index.html')
 }
 
-carlo.launch().then(async app => {
+const channel = cli.flags.channel
+
+const config = {
+  channel: [channel]
+}
+
+carlo.launch(config).then(async app => {
   app.on('exit', () => process.exit());
   app.serveFolder(__dirname);
-  app.setIcon('icon.png')
+  app.setIcon(path.join(__dirname, 'icon.png'))
   await app.exposeFunction('env', _ => process.env);
   await app.load(url);
 });
